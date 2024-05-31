5785a7ac8e61b545ed853aada19527c9bd1a2da0
Mihailqwefr <babanovmiail29@gmail.com>
2024-05-31 12:47:23 +0300

    Добавьте файлы проекта.

---
+++ proekt/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<div class="product">
+            <h2>Название книги 3</h2>
+            <p>Автор: Автор 3</p>
+            <p class="price">Цена: 700 руб.</p>
+            <button class="buy-button" onclick="addToCart('Название книги 3')">Купить</button>
+        </div>
